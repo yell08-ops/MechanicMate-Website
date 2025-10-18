138b4445b513113b27ff7be342ff2a7d88a0990d
yell08-ops <mariellelorenzo05@gmail.com>
2025-10-18 19:20:15 +0800

    Add files via upload
    Initial upload of MechanicMate website

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("bookingForm").addEventListener("submit", function(e){
+  e.preventDefault();
+  alert("Thank you! Your booking request has been submitted. Our mechanic will contact you shortly.");
+});
